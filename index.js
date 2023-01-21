a2451a1bd4dcd8331ad242e168c8f8c79e586054
myktybek <myktybek.azhimatov@gmail.com>
2023-01-21 11:17:57 -0500

    commit tree 7f0049e5e0055eaa2c558fd4f38da78a542d4761

---
+++ index.js
@@ -0,0 +1,15 @@
+ // function(introduction) {
+  // function() {
+    // string("Hi, my name is ${name}");
+  // }
+  // function introduction {
+   // return string
+// }}
+
+
+ 
+
+
+function introduction (name) {
+  console.log ('Hi, my name is'(name)('.'))
+}
